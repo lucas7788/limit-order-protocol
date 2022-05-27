e5b0fd59900f9b8a0411dc5b85ed0ff22578c292
lucas7788 <sishsh@163.com>
2022-05-27 16:19:51 +0800

    add polygon

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,18 @@ require('solidity-coverage');
 require('hardhat-deploy');
 require('hardhat-gas-reporter');
 require('dotenv').config();
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-const { ownerPrivateKey, privateKey, address ,privateKeyTest} = require('./secrets.json');
+const {
+    polygonApiKey,
+    ownerPrivateKey,
+    avaxApiKey,
+    fantomApiKey,
+    privateKey,
+    privateKey2,
+    address,
+    privateKeyTest,
+} = require('./secrets.json');
 
 module.exports = {
     solidity: {
@@ -23,9 +32,34 @@ module.exports = {
         // Obtain one at https://bscscan.com/
         //bsc  QQQ3E4UT98E7V6AAFRPEMYD21M1VG4HNHY
         //bsc_test   d87255a6627542eba4eaa9d5278832e0
-        apiKey: "QQQ3E4UT98E7V6AAFRPEMYD21M1VG4HNHY"
+        //QQQ3E4UT98E7V6AAFRPEMYD21M1VG4HNHY
+        apiKey: polygonApiKey,
     },
     networks: {
+        polygon_mainnet: {
+            url: 'https://rpc-mainnet.maticvigil.com',
+            chainId: 137,
+            timeout: 10000000,
+            accounts: [privateKey, privateKey2],
+        },
+        avax_mainnet: {
+            url: 'https://api.avax.network/ext/bc/C/rpc',
+            chainId: 43114,
+            timeout: 10000000,
+            accounts: [privateKey, privateKey2],
+        },
+        fantom_mainnet: {
+            url: 'https://rpc.ftm.tools/',
+            chainId: 250,
+            timeout: 10000000,
+            accounts: [privateKey, privateKey2],
+        },
+        eth_mainnet: {
+            url: 'https://mainnet.infura.io/v3/d87255a6627542eba4eaa9d5278832e0',
+            chainId: 250,
+            timeout: 10000000,
+            accounts: [privateKey, privateKey2],
+        },
         bsc: {
             url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
             chainId: 97,
